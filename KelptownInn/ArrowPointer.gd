fab292b5025ce86576483b800aa03acfcf3df4ad
Rubin McLuen <rub@Rubins-MacBook-Pro.local>
2025-06-12 19:06:18 -0500

    update

--- KelptownInn/ArrowPointer.gd
+++ KelptownInn/ArrowPointer.gd
@@ -1,54 +1,12 @@
 extends Sprite2D
 
-@export var target: Node2D
-@export var y_offset: float = -58.0
-@export var margin: float = 10.0
-@export var bob_amplitude: float = 2.0
-@export var bob_speed: float = 3.0
-
-var camera: Camera2D
-var t: float = 0.0
+@export var target   : Node2D
+@export var y_offset : float = -24.0     # tweak for your art
 
 func _ready() -> void:
-        camera = get_viewport().get_camera_2d()
+	# Ensure sprite is drawn around its centre (must also tick “Centered” in Inspector)
+	set_centered(true)
 
-func _process(delta: float) -> void:
-        if not camera:
-                camera = get_viewport().get_camera_2d()
-        if not target or not camera:
-                return
-
-        t += delta
-        var bob = sin(t * bob_speed) * bob_amplitude
-
-        var viewport_size = get_viewport_rect().size
-        var half_extents = viewport_size * 0.5 * camera.zoom.x
-        var left = camera.global_position.x - half_extents.x
-        var right = camera.global_position.x + half_extents.x
-        var top = camera.global_position.y - half_extents.y
-        var bottom = camera.global_position.y + half_extents.y
-
-        var desired = target.global_position + Vector2(0, y_offset + bob)
-        var pos = desired
-        var offscreen = false
-
-        if pos.x < left:
-                pos.x = left + margin
-                offscreen = true
-        elif pos.x > right:
-                pos.x = right - margin
-                offscreen = true
-
-        if pos.y < top:
-                pos.y = top + margin
-                offscreen = true
-        elif pos.y > bottom:
-                pos.y = bottom - margin
-                offscreen = true
-
-        global_position = pos
-
-        if offscreen:
-                rotation = (target.global_position - global_position).angle() - Vector2.DOWN.angle()
-        else:
-                rotation = 0.0
+func _process(_delta: float) -> void:
+	if target:
+		global_position = target.global_position + Vector2(0, y_offset)
--- KelptownInn/KelptownInnTutorial.gd
+++ KelptownInn/KelptownInnTutorial.gd
@@ -1,104 +1,114 @@
+# KelptownInnTutorial.gd
 extends "res://KelptownInn/KelptownInn.gd"
 class_name KelptownInnTutorial
 
-@onready var fade_rect: ColorRect = $CanvasLayer/FadeRect
-@onready var hint_keys: RichTextLabel = $CanvasLayer/HintMoveKeys
-@onready var hint_mouse: RichTextLabel = $CanvasLayer/HintMoveMouse
-@onready var hint_bartender: RichTextLabel = $CanvasLayer/HintBartender
-@onready var hint_hire: RichTextLabel = $CanvasLayer/HintHireBarnaby
-@onready var barnaby: NPC = $Barnaby
-@onready var arrow: Sprite2D = $Arrow
+@onready var fade_rect      : ColorRect     = $CanvasLayer/FadeRect
+@onready var hint_keys      : RichTextLabel = $CanvasLayer/HintMoveKeys
+@onready var hint_mouse     : RichTextLabel = $CanvasLayer/HintMoveMouse
+@onready var hint_bartender : RichTextLabel = $CanvasLayer/HintBartender
+@onready var hint_hire      : RichTextLabel = $CanvasLayer/HintHireBarnaby
+@onready var barnaby        : NPC           = $Barnaby
+@onready var arrow          : Sprite2D      = $CanvasLayer/Arrow
 
-var moved_keys: bool = false
-var moved_mouse: bool = false
-var stage_two_started: bool = false
+var moved_keys: bool       = false
+var moved_mouse: bool      = false
+var stage_two_started: bool   = false
 var stage_three_started: bool = false
-var _orig_speed: float = 0.0
-var tutorial_complete: bool = false
+var _orig_speed: float     = 0.0
+var tutorial_complete: bool   = false
 
 func _ready() -> void:
-		super._ready()
+	super._ready()
 
-		# Replace base bartender connection to avoid duplicate dialogue
-		if bartender.dialogue_requested.is_connected(_on_bartender_dialogue_requested):
-				bartender.dialogue_requested.disconnect(_on_bartender_dialogue_requested)
-		if bartender.dialogue_requested.is_connected(_on_bartender_dialogue_requested_tutorial):
-				bartender.dialogue_requested.disconnect(_on_bartender_dialogue_requested_tutorial)
-		bartender.dialogue_requested.connect(_on_bartender_dialogue_requested_tutorial)
+	# Avoid duplicate base connections
+	if bartender.dialogue_requested.is_connected(_on_bartender_dialogue_requested):
+		bartender.dialogue_requested.disconnect(_on_bartender_dialogue_requested)
+	if bartender.dialogue_requested.is_connected(_on_bartender_dialogue_requested_tutorial):
+		bartender.dialogue_requested.disconnect(_on_bartender_dialogue_requested_tutorial)
+	bartender.dialogue_requested.connect(_on_bartender_dialogue_requested_tutorial)
 
-		barnaby.npc_hired.connect(_on_barnaby_hired_tutorial)
-		fade_rect.modulate.a = 1.0
-		arrow.visible = false
-		hint_bartender.visible = false
-		hint_hire.visible = false
-		hint_keys.visible = true
-		hint_mouse.visible = true
+	barnaby.npc_hired.connect(_on_barnaby_hired_tutorial)
 
-		# Fade in from black
-		var tween = get_tree().create_tween()
-		tween.tween_property(fade_rect, "modulate:a", 0.0, 2.0)
+	fade_rect.modulate.a = 1.0
+	arrow.visible       = false
+	hint_bartender.visible = false
+	hint_hire.visible      = false
+	hint_keys.visible      = true
+	hint_mouse.visible     = true
 
-		# Move the player down the stairs slowly
-		_orig_speed = player.speed
-		player.speed *= 0.5
-		player.connect("auto_move_completed", Callable(self, "_on_intro_move_completed"), CONNECT_ONE_SHOT)
-		player.auto_move_to_position(Vector2(382, 88))
+	# Fade in from black
+	var tween = get_tree().create_tween()
+	tween.tween_property(fade_rect, "modulate:a", 0.0, 2.0)
+
+	# Move the player down the stairs slowly
+	_orig_speed = player.speed
+	player.speed *= 0.5
+	player.connect("auto_move_completed",
+		Callable(self, "_on_intro_move_completed"),
+		CONNECT_ONE_SHOT)
+	player.auto_move_to_position(Vector2(382, 88))
 
 func _process(_delta: float) -> void:
-		if not moved_keys and (
-				Input.is_action_pressed("ui_up") or
-				Input.is_action_pressed("ui_down") or
-				Input.is_action_pressed("ui_left") or
-				Input.is_action_pressed("ui_right")
-		):
-				moved_keys = true
-				hint_keys.add_theme_color_override("default_color", Color.GREEN)
-				_check_movement_complete()
+	if not moved_keys and (
+		Input.is_action_pressed("ui_up") or
+		Input.is_action_pressed("ui_down") or
+		Input.is_action_pressed("ui_left") or
+		Input.is_action_pressed("ui_right")
+	):
+		moved_keys = true
+		hint_keys.add_theme_color_override("default_color", Color.GREEN)
+		_check_movement_complete()
 
-		if not moved_mouse and player.mouse_move_active:
-				moved_mouse = true
-				hint_mouse.add_theme_color_override("default_color", Color.GREEN)
-				_check_movement_complete()
+	if not moved_mouse and player.mouse_move_active:
+		moved_mouse = true
+		hint_mouse.add_theme_color_override("default_color", Color.GREEN)
+		_check_movement_complete()
 
 func _check_movement_complete() -> void:
-               if moved_keys and moved_mouse and not stage_two_started:
-                               stage_two_started = true
-                               await get_tree().create_timer(1.0).timeout
-                               hint_keys.visible = false
-                               hint_mouse.visible = false
-                               arrow.target = bartender
-                               arrow.y_offset = -10.0
-                               arrow.visible = true
-                               hint_bartender.visible = true
+	if moved_keys and moved_mouse and not stage_two_started:
+		stage_two_started = true
+		await get_tree().create_timer(1.0).timeout
+		hint_keys.visible  = false
+		hint_mouse.visible = false
+
+		# Manually position arrow above the bartender
+		arrow.global_position = bartender.global_position + Vector2(0, -10)
+		arrow.visible = true
+		hint_bartender.visible = true
 
 func _on_intro_move_completed() -> void:
-		player.speed = _orig_speed
+	player.speed = _orig_speed
 
 func _on_bartender_dialogue_requested_tutorial(section: String) -> void:
-		arrow.visible = false
-		hint_bartender.add_theme_color_override("default_color", Color.GREEN)
-		player.disable_user_input = true
-		var balloon = DialogueManager.show_dialogue_balloon(
-				bartender_dialogue_resource, section, [bartender]
-		)
-		balloon.connect("dialogue_finished", Callable(self, "_on_dialogue_finished_tutorial"))
+	arrow.visible = false
+	hint_bartender.add_theme_color_override("default_color", Color.GREEN)
+	player.disable_user_input = true
+
+	var balloon = DialogueManager.show_dialogue_balloon(
+		bartender_dialogue_resource, section, [bartender]
+	)
+	# You can await the balloon closing or use its own signal; for simplicity we
+	# assume it emits "dialogue_finished" on the manager and connect accordingly:
+	balloon.connect("balloon_closed",
+		Callable(self, "_on_dialogue_finished_tutorial"))
 
 func _on_dialogue_finished_tutorial() -> void:
-                hint_bartender.visible = false
-                player.disable_user_input = false
-                stage_three_started = true
-                arrow.target = barnaby
-                arrow.y_offset = -58.0
-                arrow.visible = true
-                hint_hire.visible = true
+	hint_bartender.visible = false
+	player.disable_user_input = false
+	stage_three_started = true
 
-func _on_exit_body_entered(body: Node) -> void:
-		if not tutorial_complete:
-				return
-		super._on_exit_body_entered(body)
+	# Manually position arrow above Barnaby
+	arrow.global_position = barnaby.global_position + Vector2(0, -58)
+	arrow.visible = true
+	hint_hire.visible = true
 
 func _on_barnaby_hired_tutorial(_b: NPC) -> void:
-		hint_hire.add_theme_color_override("default_color", Color.GREEN)
-		hint_hire.visible = false
-		arrow.visible = false
-		tutorial_complete = true
+	hint_hire.add_theme_color_override("default_color", Color.GREEN)
+	hint_hire.visible = false
+	arrow.visible = false
+	tutorial_complete = true
+
+func _on_exit_body_entered(body: Node) -> void:
+	if not tutorial_complete:
+		return
+	super._on_exit_body_entered(body)
