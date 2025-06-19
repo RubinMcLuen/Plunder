72e8e4ecfe9a808516f43b3048b59ae022e58a84
Rubin McLuen <rub@Rubins-MacBook-Pro.local>
2025-06-19 18:28:16 -0500

    u

--- Battle/RangeCircle.gd
+++ Battle/RangeCircle.gd
@@ -6,8 +6,8 @@ var outline_color: Color = Color(1,1,1,1)
 var outline_width: float = 2.0
 
 func _ready() -> void:
-    update()
+	update()
 
 func _draw() -> void:
-    draw_circle(Vector2.ZERO, radius, fill_color)
-    draw_arc(Vector2.ZERO, radius, 0.0, TAU, 64, outline_color, outline_width)
+	draw_circle(Vector2.ZERO, radius, fill_color)
+	draw_arc(Vector2.ZERO, radius, 0.0, TAU, 64, outline_color, outline_width)
