0fb54148d06a72f914756c1b531c909cb19524f5
Rubin McLuen <rub@Rubins-MacBook-Pro.local>
2025-06-19 15:01:15 -0500

    u

--- Island/IslandTutorial.gd
+++ Island/IslandTutorial.gd
@@ -1,13 +1,13 @@
 extends "res://Island/island.gd"
 
 func _on_exit_body_entered(body: Node) -> void:
-    if body == player:
-        SceneSwitcher.switch_scene(
-            "res://KelptownInn/KelptownInnTutorial.tscn",
-            Vector2(269, 220),
-            "fade",
-            Vector2.ONE,
-            Vector2.ZERO,
-            Vector2(1.5, 1.5)
-        )
+	if body == player:
+		SceneSwitcher.switch_scene(
+			"res://KelptownInn/KelptownInnTutorial.tscn",
+			Vector2(269, 220),
+			"fade",
+			Vector2.ONE,
+			Vector2.ZERO,
+			Vector2(1.5, 1.5)
+		)
 
