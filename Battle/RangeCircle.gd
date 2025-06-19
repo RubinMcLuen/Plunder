8a2039b38fc418e6c38251cefdc174dd0e72f863
RubinMcLuen <138931687+RubinMcLuen@users.noreply.github.com>
2025-06-19 18:23:49 -0500

    Fix boarding battle tutorial visuals

---
+++ Battle/RangeCircle.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var radius: float = 20.0
+var fill_color: Color = Color(1,1,1,0.4)
+var outline_color: Color = Color(1,1,1,1)
+var outline_width: float = 2.0
+
+func _ready() -> void:
+    update()
+
+func _draw() -> void:
+    draw_circle(Vector2.ZERO, radius, fill_color)
+    draw_arc(Vector2.ZERO, radius, 0.0, TAU, 64, outline_color, outline_width)
