f478842a027bbb21bee0d7843f9b192ebb083daf
Rubin McLuen <rub@Rubins-MacBook-Pro.local>
2025-06-20 21:03:19 -0500

    u

--- Island/ShipControlAreaTutorial.gd
+++ Island/ShipControlAreaTutorial.gd
@@ -3,7 +3,7 @@ extends "res://Island/ShipControlArea.gd"
 func _on_button_pressed() -> void:
 	SceneSwitcher.switch_scene(
 		"res://Ocean/oceantutorial.tscn",
-		Vector2(-2, 41),
+		Vector2(-2, 39),
 		"zoom",
 		Vector2(0.0625, 0.0625),
 		Vector2(-32, 656)
